f1ad37e56799da217a804bfacfd94fb4f2ea82d7
kmlbss <49821804+kmlbss@users.noreply.github.com>
2022-09-08 00:04:36 +0400

    initial commit

---
+++ src/components/UI/Card.js
@@ -0,0 +1,12 @@
+import "./Card.css";
+
+const Card = (props) => {
+  const clases=" card " + props.className;
+  return(
+      <div className={clases}>
+          {props.children}
+      </div>
+  )
+}
+
+export default Card;
